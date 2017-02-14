fef460cf012253838a3ee5161eaa9307da48a716
cafeunder <cafe3641@gmail.com>
2017-02-14 23:59:46 +0900

    initial commit

---
+++ bounding_rect.py
@@ -0,0 +1,67 @@
+import cv2
+import os
+
+large_value = 10000000
+# 白を除いた場合の最小矩形領域を計算する
+# ひどすぎるのでなんとかしたい
+def calcRect(img):
+    # 背景は(255,255,255)である前提でグレースケール化
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    left = large_value
+    for y in range(len(gray)):
+        max_x = -1
+        for x in range(len(gray[0])):
+            if gray[y][x] == 255 and max_x < x:
+                max_x = x
+            else:
+                break
+        if not max_x == -1 and max_x < left:
+            left = max_x
+
+    top = large_value
+    for x in range(len(gray[0])):
+        max_y = -1
+        for y in range(len(gray)):
+            if gray[y][x] == 255 and max_y < y:
+                max_y = y
+            else:
+                break
+        if not max_y == -1 and max_y < top:
+            top = max_y
+
+    right = -1
+    for y in range(len(gray)):
+        min_x = large_value
+        for x in range(len(gray[0]))[::-1]:
+            if gray[y][x] == 255 and min_x > x:
+                min_x = x
+            else:
+                break
+        if not min_x == large_value and min_x > right:
+            right = min_x
+
+    bottom = -1
+    for x in range(len(gray[0])):
+        min_y = large_value
+        for y in range(len(gray))[::-1]:
+            if gray[y][x] == 255 and min_y > y:
+                min_y = y
+            else:
+                break
+        if not min_y == large_value and min_y > bottom:
+            bottom = min_y
+
+    return left, top, (right - left), (bottom - top)
+
+files = os.listdir("png")
+# すべての画像を読み込み、最小の矩形領域を抽出
+for filename in files:
+    root, ext = os.path.splitext(path)
+    if not ext == "png": continue
+
+    img = cv2.imread("png/" + filename)
+    print(finename)
+    x, y, width, height = calcRect(img)
+    dst = img[y:y + height, x:x + width]
+    cv2.imwrite("img/" + filename, dst)
---
+++ gif_to_png.py
@@ -0,0 +1,37 @@
+import cv2
+import os
+import re
+import time
+
+# gif画像の全フレームを読み込む
+def read_gif_all(filename):
+    gif = cv2.VideoCapture(filename)
+    result = list()
+    while(True):
+        _, img = gif.read()
+        if img is None: break
+        result.append(img)
+    return result
+
+count = 0
+files = os.listdir('gif')
+for filename in files:
+    # 拡張子を除いた画像名を取得
+    imgname, _ = os.path.splitext(filename)
+
+    name_form = imgname.split("-")
+    # フォルムを表す部分があるなら
+    if len(name_form) >= 2:
+        # フォルムを表す部分が通し番号 or 性別なら無視する
+        ignore_form_pattern = r"[0-9]|^m$|^f$"
+        if re.match(ignore_form_pattern, name_form[1]):
+            imgname = name_form[0]
+
+    print(imgname)
+    # gifの全フレームを読み込んで画像配列を作成
+    imgList = read_gif_all("gif/" + filename)
+
+    for img in imgList:
+        # countは重複回避のためにつけられるため、値自体に意味はない
+        cv2.imwrite("png/" + imgname + "_" + str(count) + ".png", img)
+        count += 1
