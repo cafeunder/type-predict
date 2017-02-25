b9df3bee2b23961a2c9966a1e31495182ffe115b
cafeunder <cafe3641@gmail.com>
2017-02-25 13:46:08 +0900

    bounding_rectを修正

--- src/bounding_rect.py
+++ src/bounding_rect.py
@@ -1,8 +1,9 @@
 # coding:utf-8
 '''
 pngディレクトリにある画像から、白を除いた最小矩形の画像を生成する
+画像は、ノイズが無く境界が閉じているpng画像とする
 '''
-
+import argparse
 import numpy as np
 import cv2
 import os
@@ -21,15 +22,31 @@ def calcRect(img):
 
     return left, top, (right - left), (bottom - top)
 
-files = os.listdir("../test")
-# すべての画像を読み込み、最小の矩形領域を抽出
-for filename in files:
-    base, ext = os.path.splitext(filename)
-    if not (ext == ".png" or ext == ".jpg" or ext == ".jpeg"): continue
 
-    img = cv2.imread("../test/" + filename)
-    print(filename)
-    x, y, width, height = calcRect(img)
-    dst = img[y:y + height, x:x + width]
-    os.remove("../test/" + filename)
-    cv2.imwrite("../test/" + os.path.splitext(filename)[0] + ".png", dst)
+def main():
+    parser = argparse.ArgumentParser(description='Clip the image to the smallest bounding-box')
+    parser.add_argument('--imgdir', help='Path to directory of image')
+    parser.add_argument('--clipdir', default="../clip_img", help='Path to cliped image file')
+    args = parser.parse_args()
+
+    if args.imgdir is None or not os.path.exists(args.imgdir):
+        print(str(args.imgdir) + ": invalid value of --imgdir")
+        parser.print_help()
+        return
+    if not os.path.exists(args.clipdir):
+        os.makedirs(args.clipdir)
+
+    files = os.listdir(args.imgdir)
+    # すべての画像を読み込み、最小の矩形領域を抽出
+    for filename in files:
+        base, ext = os.path.splitext(filename)
+        if not ext == ".png": continue
+
+        img = cv2.imread(args.imgdir + "/" + filename)
+        x, y, width, height = calcRect(img)
+        dst = img[y:y + height, x:x + width]
+        cv2.imwrite(args.clipdir + "/" + os.path.splitext(filename)[0] + ".png", dst)
+
+
+if __name__ == "__main__":
+    main()
