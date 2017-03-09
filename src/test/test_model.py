498f40411abce04fad6280d9710b0165efc5c882
AbeKenshi <abeken1224@yahoo.co.jp>
2017-03-09 21:33:39 +0900

    タイプ1と2を同時にテストできるように変更

--- src/test/test_model.py
+++ src/test/test_model.py
@@ -17,7 +17,6 @@ def preprocess_image(path, mean, insize):
     width, height = np.shape(img)[0], np.shape(img)[1]
     rate = float(insize) / min(width, height)
     img = cv2.resize(img, (int(width * rate), int(height * rate)))
-    print(np.shape(img))
     image = np.asarray(img, dtype=np.float32)
     if image.ndim == 2:
         # 画像の次元数が2の場合，1次元分足す
@@ -35,10 +34,27 @@ def preprocess_image(path, mean, insize):
     return image
 
 
+def make_model(model_path, learn_type):
+    """
+    学習済みモデルの読み込みを行うメソッド．
+    :param model_path: 学習済みモデルのパス
+    :param learn_type: テストするタイプの番号
+    :return: 読み込んだ番号
+    """
+    # 学習済みモデルの読み込み
+    if learn_type == 1:
+        out_size = 18
+    else:
+        out_size = 19
+    model = alexnet.Alex(out_size, False)
+    model = L.Classifier(model)
+    chainer.serializers.load_npz(model_path + "_type" + str(learn_type),
+                                 model)
+    return model, out_size
+
+
 def main():
     parser = argparse.ArgumentParser(description='Test Learned Model')
-    parser.add_argument('--learn_type', type=int, default=1,
-                        help='Type number to learn')
     parser.add_argument('--model', default='../../model_final',
                         help='Path to learned model')
     parser.add_argument('--mean', '-o', default='../../mean.npy',
@@ -49,33 +65,39 @@ def main():
     args = parser.parse_args()
 
     # 学習済みモデルの読み込み
-    if args.learn_type == 1:
-        out_size = 18
-    else:
-        out_size = 19
-    model = alexnet.Alex(out_size, False)
-    model = L.Classifier(model)
-    chainer.serializers.load_npz(args.model + "_type" + str(args.learn_type),
-                                 model)
+    model_type1, out_size_type1 = make_model(args.model, 1)
+    model_type2, out_size_type2 = make_model(args.model, 2)
 
     # 平均画像の読み込み
     mean = np.load(args.mean)
-
-    # 画像からタイプを予測
-    y = model.predictor(
-        np.array([preprocess_image(args.img, mean, 224)]))
-    y = y.data
-    y = np.exp(y) / np.sum(np.exp(y))  # ソフトマックス関数で各タイプの確率を計算
 
     # 推定されたタイプを出力
     type_file = open(args.label, 'r')
     type_list = type_file.read().split("\n")
 
-    for i in range(out_size):
+    # 画像からタイプを予測
+    y_type1 = model_type1.predictor(
+        np.array([preprocess_image(args.img, mean, 224)]))
+    y_type1 = y_type1.data
+    y_type1 = np.exp(y_type1) / np.sum(np.exp(y_type1))  # ソフトマックス関数で各タイプの確率を計算
+
+    for i in range(out_size_type1):
         if type_list[i] == "":
             break
-        print(type_list[i], ":", str(int(y[0][i] * 100)), "%")
-    print("Type of this image is : " + type_list[np.argmax(y)])
+        print(type_list[i], ":", str(int(y_type1[0][i] * 100)), "%")
+    print("Type1 of this image is : " + type_list[np.argmax(y_type1)])
+
+    # 画像からタイプを予測
+    y_type2 = model_type2.predictor(
+        np.array([preprocess_image(args.img, mean, 224)]))
+    y_type2 = y_type2.data
+    y_type2 = np.exp(y_type2) / np.sum(np.exp(y_type2))  # ソフトマックス関数で各タイプの確率を計算
+
+    for i in range(out_size_type2):
+        if type_list[i] == "":
+            break
+        print(type_list[i], ":", str(int(y_type2[0][i] * 100)), "%")
+    print("Type2 of this image is : " + type_list[np.argmax(y_type2)])
 
 
 if __name__ == '__main__':
