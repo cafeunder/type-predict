7ffdb29accff05c7ad559546e823d541bff360bb
AbeKenshi <abeken1224@yahoo.co.jp>
2017-03-28 21:19:34 +0900

    テスト時の画像読み込み方法を変更

--- src/test.py
+++ src/test.py
@@ -7,27 +7,35 @@ import argparse
 import numpy as np
 import chainer
 import chainer.links as L
+import chainer.functions as F
 import cv2
 from PIL import Image
 from models import alexnet
 
 def preprocess_image(path, mean, insize):
-    img = cv2.imread(path)
-    width, height = np.shape(img)[0], np.shape(img)[1]
-    rate = float(insize) / min(width, height)
-    img = cv2.resize(img, (int(width * rate), int(height * rate)))
-    image = np.asarray(img, dtype=np.float32)
+    mean = mean.astype('f')
+    f = Image.open(path)
+    try:
+        image = np.asarray(f, np.float32)
+    finally:
+        if hasattr(f, 'close'):
+            f.close()
+    # img = cv2.imread(path)
+    # width, height = np.shape(img)[0], np.shape(img)[1]
+    # rate = float(insize) / min(width, height)
+    # img = cv2.resize(img, (int(width * rate), int(height * rate)))
+    # image = np.asarray(img, dtype=np.float32)
     if image.ndim == 2:
         # 画像の次元数が2の場合，1次元分足す
         image = image[:, :, np.newaxis]
     image = image.transpose(2, 0, 1)
 
-    _, h, w = image.shape
-    top = (h - insize) // 2
-    left = (w - insize) // 2
-    bottom = top + insize
-    right = left + insize
-    image = image[0:3, top:bottom, left:right]
+    # _, h, w = image.shape
+    # top = (h - insize) // 2
+    # left = (w - insize) // 2
+    # bottom = top + insize
+    # right = left + insize
+    # image = image[0:3, top:bottom, left:right]
     image -= mean
     image *= (1.0 / 255.0)
     return image
@@ -77,10 +85,11 @@ def main():
     type_list = type_file.read().split("\n")
 
     # 画像からタイプを予測
-    y_type1 = model_type1.predictor(
-        np.array([preprocess_image(args.img, mean1, 224)]))
-    y_type1 = y_type1.data
-    y_type1 = np.exp(y_type1) / np.sum(np.exp(y_type1))  # ソフトマックス関数で各タイプの確率を計算
+    y_type1 = F.softmax(model_type1.predictor(np.array([preprocess_image(args.img, mean1, 224)])))
+    # y_type1 = model_type1.predictor(
+    #     np.array([preprocess_image(args.img, mean1, 224)]))
+    # y_type1 = y_type1.data
+    # y_type1 = np.exp(y_type1) / np.sum(np.exp(y_type1))  # ソフトマックス関数で各タイプの確率を計算
 
     for i in range(out_size_type1):
         if type_list[i] == "":
@@ -89,10 +98,11 @@ def main():
     print("Type1 of this image is : " + type_list[np.argmax(y_type1)])
 
     # 画像からタイプを予測
-    y_type2 = model_type2.predictor(
-        np.array([preprocess_image(args.img, mean2, 224)]))
-    y_type2 = y_type2.data
-    y_type2 = np.exp(y_type2) / np.sum(np.exp(y_type2))  # ソフトマックス関数で各タイプの確率を計算
+    y_type2 = F.softmax(model_type2.predictor(np.array([preprocess_image(args.img, mean2, 224)])))
+    # y_type2 = model_type2.predictor(
+    #     np.array([preprocess_image(args.img, mean2, 224)]))
+    # y_type2 = y_type2.data
+    # y_type2 = np.exp(y_type2) / np.sum(np.exp(y_type2))  # ソフトマックス関数で各タイプの確率を計算
 
     for i in range(out_size_type2):
         if type_list[i] == "":
